c45db990292c5963574e5cb4f05725ed0cf74272
Denis Kaysarov <kaysik_it@mail.ru>
2022-08-31 19:44:05 +0500

    recursive output of reviews

--- movies/models.py
+++ movies/models.py
@@ -131,7 +131,12 @@ class Review(models.Model):
     email = models.EmailField(max_length=150)
     name = models.CharField("Имя", max_length=100)
     text = models.TextField("Сообщение", max_length=5000)
-    parent = models.ForeignKey('self', verbose_name="Родитель", on_delete=models.SET_NULL, null=True, blank=True)
+    parent = models.ForeignKey(
+        'self',
+        verbose_name="Родитель",
+        on_delete=models.SET_NULL,
+        null=True, blank=True,
+        related_name="children")
     movie = models.ForeignKey(Movie, verbose_name="фильм", on_delete=models.CASCADE, related_name="reviews")
 
     def __str__(self):
