c4949eb5287520cbfa506767e2f79d2e2734cb19
Denis Kaysarov <kaysik_it@mail.ru>
2022-08-31 19:20:52 +0500

    Add adding and displaying movie reviews

--- movies/models.py
+++ movies/models.py
@@ -132,7 +132,7 @@ class Review(models.Model):
     name = models.CharField("Имя", max_length=100)
     text = models.TextField("Сообщение", max_length=5000)
     parent = models.ForeignKey('self', verbose_name="Родитель", on_delete=models.SET_NULL, null=True, blank=True)
-    movie = models.ForeignKey(Movie, verbose_name="фильм", on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, verbose_name="фильм", on_delete=models.CASCADE, related_name="reviews")
 
     def __str__(self):
         return f"{self.name} - {self.movie}"
